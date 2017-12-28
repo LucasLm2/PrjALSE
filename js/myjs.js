5b4f46584b83cb6e2f7509bc7b9dbb1e26245c5d
LucasLm2 <lucaslm2>
2017-12-28 18:18:05 -0300

    28/12/2017 18:17

--- js/myjs.js
+++ js/myjs.js
@@ -4,12 +4,31 @@ jQuery(function () {
 	jQuery(window).scroll(function () {
 		if (jQuery(this).scrollTop() > 50) {
 			$("#menu").removeClass("navbar-dark");
-			$("#menu").addClass("bg-light navbar-orange-red");
+			$("#menu").addClass("bg-grey navbar-orange-red");
 			$("#logo").attr("src", "img/logoblack.png");
 		} else {
-			$("#menu").removeClass("bg-light navbar-orange-red");
+			$("#menu").removeClass("bg-grey navbar-orange-red");
 			$("#menu").addClass("navbar-dark");
 			$("#logo").attr("src", "img/logo.png");
 		}
 	});
 });
+
+/*jQuery(function (a){
+	"use strict";
+	a('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+		if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+			var e = a(this.hash);
+			if ((e = e.length ? e : a("[name=" + this.hash.slice(1) + "]")).length) { 
+				return a("html, body").animate({ scrollTop: e.offset().top - 100}, 1e3, "easeInOutExpo"), !1;
+			}
+		}
+	}), 
+	a(".js-scroll-trigger").click(function () {
+		a(".navbar-collapse").collapse("hide");
+	}),
+	a("body").scrollspy({
+		target: "#menu",
+		offset: 100
+	});
+});*/
