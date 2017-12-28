447254a24f609f893e08360e7976687f8ea68755
LucasLm2 <lucaslm2>
2017-12-28 11:17:42 -0300

    Arrumando a logo

--- js/myjs.js
+++ js/myjs.js
@@ -4,19 +4,12 @@ jQuery(function () {
 	jQuery(window).scroll(function () {
 		if (jQuery(this).scrollTop() > 50) {
 			$("#menu").removeClass("navbar-dark");
-			$("#menu").addClass("bg-white navbar-orange-red");
+			$("#menu").addClass("bg-light navbar-orange-red");
 			$("#logo").attr("src", "img/logoblack.png");
 		} else {
-			$("#menu").removeClass("bg-white navbar-orange-red");
+			$("#menu").removeClass("bg-light navbar-orange-red");
 			$("#menu").addClass("navbar-dark");
 			$("#logo").attr("src", "img/logo.png");
 		}
 	});
 });
-
-function googleTranslateElementInit() {
-	new google.translate.TranslateElement({
-		pageLanguage: 'en',
-		layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-	}, 'google_translate_element');
-}
